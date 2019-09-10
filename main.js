29d0e64e83a3001fff9547e2aa9754fc1647b708
Morrism1 <morrismontana0@gmail.com>
2019-09-10 03:32:38 -0700

    add menu

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app,BrowserWindow} = electron;
+const {app,BrowserWindow,Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 //Listen for app to be ready
 app.on('ready', function(){
@@ -16,4 +17,44 @@ app.on('ready', function(){
         protocol: 'file',
         slashes: true
     }))
+// Build menu from template
+const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+//Insert menu
+Menu.setApplicationMenu(mainMenu)
 })
+// Handle add window
+function createAddWindow(){
+     //create new window
+     addWindow = new BrowserWindow({
+         width: 200,
+         height: 300,
+         title: 'Add ShoppingList Item'
+     });
+     // Load html into window
+     addWindow.loadURL(url.format({
+         pathname: path.join(__dirname,'addWindow.html'),
+         protocol: 'file',
+         slashes: true  
+}))
+}
+// Create menu template
+const mainMenuTemplate = [{
+    label: 'File',
+    submenu:[{
+        label: 'Add Item' ,
+        click(){
+            createAddWindow();
+        }
+    },
+    {
+        label: 'Clear Item'
+    },
+    {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+            app.quit(); 
+        }
+    }
+    ]
+}]
